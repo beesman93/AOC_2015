f3b85eaed7ef4a9df0e2019e9d2921929556003e
beesman93 <tomas@chvojka.dev>
2024-12-25 18:32:55 +0100

    Day4

---
+++ Solutions/Day04.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using AoCHelper;
+
+namespace AOC_2015
+{
+    internal class Day04: BaseDayWithInput
+    {
+        public Day04()
+        {
+
+        }
+        public override ValueTask<string> Solve_1()
+        {
+            MD5 md5 = MD5.Create();
+            for(int i =0;i < int.MaxValue; i++)
+            {
+                md5.ComputeHash(Encoding.ASCII.GetBytes(_input[0]+i));
+                if (md5.Hash[0] == 0 && md5.Hash[1]==0 && md5.Hash[2]<16)
+                    return new($"{i}");
+            }
+            return new($"nada");
+        }
+        public override ValueTask<string> Solve_2()
+        {
+            MD5 md5 = MD5.Create();
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                md5.ComputeHash(Encoding.ASCII.GetBytes(_input[0] + i));
+                if (md5.Hash[0] == 0 && md5.Hash[1] == 0 && md5.Hash[2] == 0)
+                    return new($"{i}");
+            }
+            return new($"nada");
+        }
+    }
+}
