8e75a404a5d8b57735271699ed05221493ef4beb
beesman93 <tomas@chvojka.dev>
2024-12-25 17:43:11 +0100

    Add Day00 sol as template for later copying

---
+++ Solutions/Day00.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AoCHelper;
+
+namespace AOC_2015
+{
+    internal class Day00: BaseDayWithInput
+    {
+        public Day00()
+        {
+
+        }
+        public override ValueTask<string> Solve_1()
+        {
+            var ans = 0;
+
+            return new($"{ans}");
+        }
+        public override ValueTask<string> Solve_2()
+        {
+            var ans = 0;
+
+            return new($"{ans}");
+        }
+    }
+}
